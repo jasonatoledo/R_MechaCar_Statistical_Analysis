32c703fcf374dea9d22db83179d51ecd9d136f76
Jason <jasonatoledo@gmail.com>
2020-10-20 23:12:04 -0700

    Updating MechaCar Challenge

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,10 +1,18 @@
 library(dplyr) # add dpylr
+
+#Deliverable 1
 df <- read.csv("MechaCar_mpg.csv",sep=",",header = TRUE) #add csv file
 view(df) #check df
 lm(mpg ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + vehicle_length, data = df) # linear regression for multiple poins
 summary(lm(mpg ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + vehicle_length, data = df)) # lm with summary function
 
+#Deliverable 2
 df2 <- read.csv("Suspension_coil.csv",sep = ",", header = TRUE) #add csv file
 total_summary <- df2 %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups='keep') #PSI Summary DF
 lot_summary <- df2 %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups='keep') #Lot Summary DF
 
+#Deliverable 3
+t.test(df2$PSI)
+t.test(subset(df2, Manufacturing_Lot == "Lot1", select = PSI))
+t.test(subset(df2, Manufacturing_Lot == "Lot2", select = PSI))
+t.test(subset(df2, Manufacturing_Lot == "Lot3", select = PSI))

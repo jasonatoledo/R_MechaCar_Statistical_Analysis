1d4db9d1bab572138b9c87fddb21aa27f28b2c2f
Jason <jasonatoledo@gmail.com>
2020-10-20 23:12:58 -0700

    Updating MechaCarChallenge

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -10,9 +10,8 @@ summary(lm(mpg ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + vehic
 df2 <- read.csv("Suspension_coil.csv",sep = ",", header = TRUE) #add csv file
 total_summary <- df2 %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups='keep') #PSI Summary DF
 lot_summary <- df2 %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups='keep') #Lot Summary DF
-
 #Deliverable 3
-t.test(df2$PSI)
-t.test(subset(df2, Manufacturing_Lot == "Lot1", select = PSI))
-t.test(subset(df2, Manufacturing_Lot == "Lot2", select = PSI))
-t.test(subset(df2, Manufacturing_Lot == "Lot3", select = PSI))
+t.test(df2$PSI, mu = 1500)
+t.test(subset(df2, Manufacturing_Lot == "Lot1", select = PSI), mu = 1500)
+t.test(subset(df2, Manufacturing_Lot == "Lot2", select = PSI), mu = 1500)
+t.test(subset(df2, Manufacturing_Lot == "Lot3", select = PSI), mu = 1500)
